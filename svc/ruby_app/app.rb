96475f9c720b2846ea89547da29811bd88fda4c2
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-06-11 20:37:19 +0800

    Update app.rb
    refactored logging directory handling in Sinatra app

--- svc/ruby_app/app.rb
+++ svc/ruby_app/app.rb
@@ -13,17 +13,23 @@ class JsonLogger < Logger
   end
 end
 
-log = JsonLogger.new(File.open("/var/log/ruby_app/app.log", "a"))
+
+log_directory = ENV['RACK_ENV'] == 'test' ? './logs' : './logs'
+
+Dir.mkdir(log_directory) unless File.exist?(log_directory)
+
+log = JsonLogger.new(File.open("#{log_directory}/app.log", "a"))
 
 set :bind, '0.0.0.0'
 set :port, 4567
 
+
 get '/' do
-  log.info("This is an info message")
+  log.info("This is an info statement")
   "Hello, world!"
 end
 
 get '/error' do
-  log.error("This is an error message")
+  log.error("This is an error statement")
   "An error occurred!"
 end
