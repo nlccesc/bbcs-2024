0fd313508f4f49f7865480df28b75e8fb09aa026
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-06-13 16:44:17 +0800

    Update app.py
    - added background thread to start a metrics server using a prometheus client (middleware management)
    
    - added a 'key' parameter to ensure state management within the streamlit app
    
    - changed the button click handling to directly call within an 'if' statement

--- app/app.py
+++ app/app.py
@@ -1,25 +1,22 @@
 import streamlit as st 
 import query_responder 
+from threading import Thread
+from middleware import start_metrics_server
 
+# Start the metrics server in a background thread
+Thread(target=start_metrics_server, daemon=True).start()
 
 # backend 
-
 q = "" 
 
 def eval_query(query): 
-    if query == "": return 
+    if query == "": 
+        return 
     st.text("Response:")
     st.text(query_responder.respond(query))
 
-
-
-
 # frontend 
-
-
 st.title("Query")
-
-q = st.text_input("Query")
-
-st.button("Ask", on_click=eval_query(q))
-
+q = st.text_input("Query", key='query_input')
+if st.button("Ask"):
+    eval_query(q)
